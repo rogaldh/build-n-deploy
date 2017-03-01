cd0cb893535ab0bf178ba4c8257c4075d3155437
Sergo <vd@radsh.red>
2017-03-01 21:55:57 +0300

    Update unadded files from previous commits

--- doc/src/nginx-docker-image/node-install.sh
+++ doc/src/nginx-docker-image/node-install.sh
@@ -1,14 +1,33 @@
 #!/bin/bash
 
-apt-get update && apt-get install -y \
-    curl;
+read -p " Which version of Node do you need to install (any valid version): " VERSIONNAME
+read -p " Are you using a 32-bit or 64-bit operating system ? Enter 64 or 32: " ARCHVALUE
+if [[ $ARCHVALUE = 32 ]]
+    then
+    printf " using 32 \n"
+    ARCHVALUE=86
+    URL=http://nodejs.org/dist/v${VERSIONNAME}/node-v${VERSIONNAME}-linux-x${ARCHVALUE}.tar.xz
 
-cd /usr/local/bin;
-curl -sL https://nodejs.org/dist/v7.6.0/node-v7.6.0-linux-x64.tar.xz > node.xz;
+elif [[ $ARCHVALUE = 64 ]]
+    then
+    printf " using 64 \n"
+    ARCHVALUE=64
+    URL=http://nodejs.org/dist/v${VERSIONNAME}/node-v${VERSIONNAME}-linux-x${ARCHVALUE}.tar.xz
+
+else
+    printf "invalid input expected either 32 or 64 as input, quitting ... \n"
+    exit
+fi
 
 # if tar can\'t untar .xz
-apt-get install -y xz-utils < "/dev/null";
-tar -xJf node.xz && rm node.xz;
-mv node-v7.6.0-linux-x64/ node;
-cd /usr/bin;
-ln -s /usr/local/bin/node/bin/node nodejs;
+apt-get update && apt-get install -y \
+    curl \
+    xz-utils \
+    < "/dev/null";
+
+cd ~ && mkdir .node && cd .node;
+curl -sL $URL | tar --strip-components 1 -xJ;
+
+if ! grep -q "~/.node/bin" ~/.bashrc; then
+  echo "export PATH=$PATH:~/.node/bin" >> ~/.bashrc;
+fi;
