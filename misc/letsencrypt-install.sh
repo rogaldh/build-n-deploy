0a3e657eab55a8f7ce646df46dd8bf1a8cdf5549
Sergo <rogaldh@radsh.red>
2017-05-17 22:33:31 +0300

    Add letsencrypt install script

---
+++ misc/letsencrypt-install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# https://certbot.eff.org/#ubuntutrusty-nginx
+
+apt-get install -y software-properties-common
+add-apt-repository ppa:certbot/certbot
+apt-get update
+apt-get install -y certbot
+
+echo "Woop! Let'sEncrypt is installed. You can simply create new cert by run: `sudo certbot certonly --standalone DOMAIN_NAME(S)`'"
