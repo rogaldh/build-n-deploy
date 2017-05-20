78704a79a1a302a6590bee80378d9b023fbdc608
Sergo <rogaldh@radsh.red>
2017-05-20 18:11:45 +0300

    Allow to specify nodejs version and stuff

--- docker-images/tarball-image/build.sh
+++ docker-images/tarball-image/build.sh
@@ -1,29 +1,38 @@
 #!/bin/bash
 
-read -p "Enter path to dir which contains files for build: " PATHTOFILES
-read -p "Enter Docker image name to build: " IMAGENAME
+read -p "Enter path to dir which contains files for build (./): " PATHTOFILES
+read -p "Enter Docker image name to build (tarball): " IMAGENAME
+read -p "Enter Node.js version (7.8): " NODEVER
 
-PATHTOFILES = ${PATHTOFILES:=./}
-IMAGENAME = ${IMAGENAME:='tarball'}
+: PATHTOFILES = ${PATHTOFILES:='./'}
+: IMAGENAME = ${IMAGENAME:='tarball'}
+: NODEVER = ${NODEVER:='7.8'}
 
 [ $PATHTOFILES == './' ] && PATHTOCOPY='../' || PATHTOCOPY='../../'
 
 # inject .gitignore
-cp .dockerignore $PATHTOFILES
+! test -f "${PATHTOFILES}.dockerignore" && cp .dockerignore $PATHTOFILES && echo "`.dockerignore` was created"
 
 # tar fallback
 if ! [ -x "$(command -v tar)" ]; then
-    sudo apt-get update && apt-get install -y tar
+  echo "Let's install tar"
+  sudo apt-get update && apt-get install -y tar
 fi
 
 # Archive node_modules
 if ! [ $(find $PATHTOFILES -type f -name 'node_modules*') ]; then
-    cd $PATHTOFILES/node_modules
-    tar -cvf ../node_modules.tar *
-    cd $PATHTOCOPY
+  echo "Let's pack modules"
+  
+  cd $PATHTOFILES/node_modules
+  tar -cvf ../node_modules.tar *
+  cd $PATHTOCOPY
+
+  echo '`node_modules` were packed'
 fi
 
-sudo docker build \
-    --build-arg SOURCEPATH=$PATHTOFILES \
-    -t $IMAGENAME \
-    .
+echo 'Start build procedure'
+docker build \
+ --build-arg SOURCEPATH=$PATHTOFILES \
+ --build-arg NODEVER=$NODEVER \
+ -t $IMAGENAME \
+ .
--- docker-images/tarball-image/readme.md
+++ docker-images/tarball-image/readme.md
@@ -1,33 +1,14 @@
-# app-release-image
+# tarball-image
 
-Contains application codebase. App should be injected on build. Works in pair with Nginx image.
-
-## Prerequisites
-Built on top of [`nodejs`](../nodejs/readme.md#L=14) docker image.
-Uses [`nginx-proxy`](../nginx-proxy/readme.md) docker image to work with.
+Suited for applications should be fully bundled inside a container.
 
 ## Notes
-[`Dockerfile`](./Dockerfile) contains kinda hack around `npm i` :\
+- [`Dockerfile`](./Dockerfile#L10) contains kinda hack around `npm i`
 
 ## How to
+Builder works ourside the application directory. That's why there is no need to store Dockerfile inside the app. The only thing to care about is a version of nodejs.
 ```
-# Build
-## Copy stuff to the directory which contains folder with an application
+# Copy stuff to the directory which contains folder with an application
 $ cp ./* PATH_TO_THE_APP_PARENT_DIRECTORY
-$ ./docker-build.sh
-
-# Run
-# Assume that we already have a server image and an app image
-
-# In most cases INTERNAL_PORT and EXTERNAL_PORT should be equal to the port which is exposed from app image
-$ sudo docker run -d \
-    -p EXTERNAL_PORT:INTERNAL_PORT \
-    --name RUNNING_IMAGE_NAME \
-    APP_IMAGE_NAME
-
-# Here we make a link to the running image by name (`RUNNING_IMAGE_NAME`). It is used at `proxy_pass` directive
-$ sudo docker run -d \
-    -p EXTERNAL_IP:EXTERNAL_PORT:80 \
-    --link RUNNING_IMAGE_NAME:app \
-    SERVER_IMAGE_NAME
+$ sudo ./build.sh
 ```
