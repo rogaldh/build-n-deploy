eef0378fd935eee400cec3ca773737a6af67ec0f
Sergo <rogaldh@radsh.red>
2017-06-23 00:20:24 +0300

    Create link to secrets & application config's dir

--- sources/reverse-proxy/include.v/run.sh
+++ sources/reverse-proxy/include.v/run.sh
@@ -9,6 +9,9 @@ read -p "Which image should be linked (app): " UPSTREAM
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # get abs path to the script
 
+! test -d "./letsencrypt" && ln -s /etc/letsencrypt letsencrypt
+# link letsencrypt dir with secrets if not exist
+
 docker run -p 80:80 -p 443:443 \
   -v ${DIR}/letsencrypt:/etc/nginx/letsencrypt \
   -v ${DIR}/default:/etc/nginx/conf.d/default \
