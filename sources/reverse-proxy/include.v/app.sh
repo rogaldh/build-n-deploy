951663dfde92b18d9b34050b2a2512c873e6d431
Sergo <rogaldh@radsh.red>
2017-06-27 13:19:38 +0300

    Add complementary app start script

---
+++ sources/reverse-proxy/include.v/app.sh
@@ -0,0 +1,17 @@
+g --name im_production  -d instrummart:7.9.0
+
+#!/bin/bash
+
+read -p "Which application-image should be runned (appimage): " IMAGE
+read -p "Which name should aplication use (app): " NAME
+
+: IMAGE = ${IMAGE:='appimage'}
+: NAME = ${NAME:='app'}
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+# get abs path to the script
+
+docker run \
+  -v ${DIR}/../config/:/usr/share/nginx/html/config \
+  --name $NAME \
+  -d $IMAGE
